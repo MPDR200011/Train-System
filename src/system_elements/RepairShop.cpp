7142e8fd72bae3fcf85eda51c7b7d2b1eb800a95
Moisés Rocha <mpdr200011@gmail.com>
2019-01-08 20:12:35 +0000

    repair shop implementation inital phase

---
+++ src/system_elements/RepairShop.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mpdr on 1/8/19.
+//
+
+#include "RepairShop.h"
---
+++ src/system_elements/RepairShop.h
@@ -0,0 +1,14 @@
+//
+// Created by mpdr on 1/8/19.
+//
+
+#ifndef TRAIN_SYSTEM_REPAIRSHOP_H
+#define TRAIN_SYSTEM_REPAIRSHOP_H
+
+
+class RepairShop {
+
+};
+
+
+#endif //TRAIN_SYSTEM_REPAIRSHOP_H
